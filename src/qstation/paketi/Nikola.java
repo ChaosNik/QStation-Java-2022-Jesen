5eadb128473fc7491638b4a323f34729b5519d44
Nikola Karpić <nikola.karpic@ttcontrol.com>
2022-11-03 20:37:14 +0100

    Komentar

--- src/qstation/paketi/Nikola.java
+++ src/qstation/paketi/Nikola.java
@@ -2,6 +2,7 @@ package qstation.paketi;
 
 public class Nikola {
 	public static void main(String[] args) {
+		// Ispisivanje godina
 		System.out.println("Dragana ima: " + Dragana.godine() + " godina.");
 		System.out.println("Dragomir ima: " + Dragomir.godine() + " godina.");
 		System.out.println("Josip ima: " + Josip.godine() + " godina.");
